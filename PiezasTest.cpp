13d377895325d09aeb439fd154d6fea24d70c2f7
Dmytro <dima220v@gmail.com>
2020-05-02 21:44:29 -0700

    reset board

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -96,3 +96,60 @@ TEST(PiezasTest, drop_invalid_first_turn)
   piece = board.dropPiece(0);
   ASSERT_EQ(piece, O);
 }
+
+
+// Piece at test
+
+TEST(PiezasTest, check_blank)
+{
+  Piezas board;
+  Piece piece;
+  piece = board.pieceAt(0,0);
+  ASSERT_EQ(piece, Blank);
+}
+
+TEST(PiezasTest, expect_x_at_pos)
+{
+  Piezas board;
+  Piece piece;
+  board.dropPiece(0);
+  piece = board.pieceAt(0,0);
+  ASSERT_EQ(piece, X);
+}
+
+TEST(PiezasTest, expect_o_at_pos)
+{
+  Piezas board;
+  Piece piece;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  piece = board.pieceAt(0,1);
+  ASSERT_EQ(piece, O);
+}
+
+TEST(PiezasTest, out_of_range_arguments)
+{
+  Piezas board;
+  Piece piece;
+  piece = board.pieceAt(7,0);
+  ASSERT_EQ(piece, Invalid);
+}
+
+
+// Reset board test
+
+TEST(PiezasTest, reset_simple)
+{
+  Piezas board;
+  for(unsigned int i=0; i < BOARD_ROWS; i++)
+    for(unsigned int j=0; j < BOARD_COLS; j++)
+		board.dropPiece(i);
+
+  board.reset();
+  for(unsigned int i=0; i < BOARD_ROWS; i++)
+    for(unsigned int j=0; j < BOARD_COLS; j++)
+      ASSERT_EQ(board.pieceAt(i,j), Blank);
+}
+
+
+// Game state
